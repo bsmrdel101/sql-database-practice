769e7b70d57feafd1469474b5ad33b8510c8a9e7
Bennett Smrdel <smrdelb@gmail.com>
2021-11-16 18:54:09 -0600

    added SQL into the database.sql file

--- database.sql
+++ database.sql
@@ -1,8 +1,16 @@
-CREATE TABLE "artists" (
+CREATE TABLE "artist" (
     "id" SERIAL PRIMARY KEY,
     "artist_name" varchar(80) not null,
     "year_born" date
 );
+
+CREATE TABLE "song" (
+	"id" SERIAL PRIMARY KEY,
+	"title" varchar(255) not null,
+	"length" varchar(10) not null,
+	"released" date
+);
+
 
 INSERT INTO "artist"
 ("name", "birthdate")
