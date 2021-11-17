66f33d585b3ca902018bc19b8f632279dabdd7b8
Bennett Smrdel <smrdelb@gmail.com>
2021-11-16 19:20:58 -0600

    connected routes to database

--- server/routes/songRouter.js
+++ server/routes/songRouter.js
@@ -1,15 +1,61 @@
 const express = require('express');
 const router = express.Router();
+// Require the pg module:
+const pg = require('pg');
+
+// Create a pool object constructor.
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'song',
+    host: 'localhost'
+  });
+
+// Log to our console when our pool object makes a connection:
+pool.on('connect', () => {
+    console.log('Postgresql connected');
+  });
+  
+  // Log to our console when something makes our pool error out:
+  pool.on('error', (error) => {
+    console.log('Error with postgres pool', error)
+  });
 
 
+
+// Get and post routes
 router.get('/', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(songList);
+    const sqlText = 'SELECT * FROM song;'
+  pool.query(sqlText)
+    .then((dbRes) => {
+      const songFromDb = dbRes.rows;
+      res.send(songFromDb)
+    }).catch((dbErr) => {
+      console.error(dbErr);
+    });
 });
 
 router.post('/', (req, res) => {
-    songList.push(req.body);
-    res.sendStatus(201);
+    const newSong = req.body;
+    const sqlText = (`
+    INSERT INTO "song"
+    "title", "length", "released")
+    VALUES
+      ($1, $2, $3);
+  `)
+  const sqlValues = [
+    newSong.title,
+    newSong.length,
+    newSong.released,
+  ]
+  console.log(sqlText)
+  pool.query(sqlText, sqlValues)
+    .then((dbRes) => {
+      res.sendStatus(201);
+    })
+    .catch((dbErr) => {
+      console.error(dbErr);
+    })
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,46 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+// Require routers
+const songRouter = require('./routes/songRouter');
+const artistRouter = require('./routes/songRouter');
+
+const artistList = [ 
+    {
+        name: 'Ella Fitzgerald',
+        birthdate: '04-25-1917'
+    },
+    {
+        name: 'Dave Brubeck',
+        birthdate: '12-06-1920'
+    },       
+    {
+        name: 'Miles Davis',
+        birthdate: '05-26-1926'
+    },
+    {
+        name: 'Esperanza Spalding',
+        birthdate: '10-18-1984'
+    },
+]
+const songList = [
+    {
+        title: 'Take Five',
+        length: '5:24',
+        released: '1959-09-29'
+    },
+    {
+        title: 'So What',
+        length: '9:22',
+        released: '1959-08-17'
+    },
+    {
+        title: 'Black Gold',
+        length: '5:17',
+        released: '2012-02-01'
+    }
+];
+
 // Routes
 app.use('/song', songRouter);
 app.use('/artist', artistRouter);
